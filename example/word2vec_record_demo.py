74859cafe64cf8b70d667720c8aab7e7c5501fb6
xuezhengyin210834 <xuezhengyin210834@sohu-inc.com>
2019-10-21 20:31:22 +0800

    word2vec生成TF Record

--- example/word2vec_record_demo.py
+++ example/word2vec_record_demo.py
@@ -5,7 +5,6 @@ import sys
 import argparse
 from os.path import abspath, dirname
 sys.path.insert(0, abspath(dirname(dirname(__file__))))
-from word2vec.Word2vecModel import Word2vecModelPipeline
 from word2vec.Word2vecTokenzier import Word2vecTokenizer as wt
 
 def main():
@@ -16,19 +15,23 @@ def main():
 
 if __name__=='__main__':
     ap = argparse.ArgumentParser(prog="Word2vec")
+    # 输入数据
     ap.add_argument("--input", help="input path of the word sequences ")
+    # 迭代轮数
     ap.add_argument("--iter", type=int, default=20, help="max iteration of the word2vec")
+    # 窗口大小
     ap.add_argument("--window_size", type=int, default=5, help="window size of the word2vec model")
+    # batch大小
     ap.add_argument("--batch_size", type=int, default=300, help="batch size of train model data")
-    ap.add_argument("--lr", type=float, default=1.0, help="learn rate of the word2vec model")
-    ap.add_argument("--size", type=int, default=128, help="dimensions size of the word embedding space")
+    # 最小出现次数
     ap.add_argument("--min_count", type=int, default=10, help="minimum word frequency")
-    ap.add_argument("--log_dir", default="C:\\Users\\xuezhengyin210834\\Desktop\\word2vec_log", help="directory of tensor board log")
+    # 采样次数
     ap.add_argument("--num_sampled", type=int, default=64, help="num sampled of the word2vec model")
+    # 输出路径
     ap.add_argument("--output", help="output path of the vector")
+    # 线程数
     ap.add_argument("--thread", type=int, default=2, help="thread number of the preprocessing")
-    ap.add_argument("--lsize", type=int, default=1000, help="logging of the batch size")
-    ap.add_argument("--timeline", default=None, help="Tensor flow time line json path")
+    # 多少个batch size保留
     ap.add_argument("--store_size", default=100, help="store tensorflow record path")
     args = vars(ap.parse_args())
     main()
